33dc00b0f83f319573e7ba06ed15b9a928aaaed2
eesphgos2 <eesphgos2@gmail.com>
2025-01-02 08:11:49 +0000

    Deploying to gh-pages from @ eesphgos2/eesphgos2.github.io@fb8cc260e0e69cb7357152839b18b3f540f9d256 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -442,9 +442,9 @@ ninja.data = [{
           description: "a king who not have son.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-jungle",
+          title: 'jungle',
+          description: "trip in the jungle",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
             },},{id: "projects-project-4",
