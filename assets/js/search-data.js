aebff622f367a6af09a219ea4bec6ccde8057a3a
eesphgos2 <eesphgos2@gmail.com>
2025-01-02 14:59:50 +0000

    Deploying to gh-pages from @ eesphgos2/eesphgos2.github.io@8ce9f7dd67035f365ff272f4718267bfc56114f6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -208,7 +208,7 @@ ninja.data = [{
         title: 'Custom_social',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://eesphgos.github.io/", "_blank");
         },
       },{
       id: 'light-theme',
