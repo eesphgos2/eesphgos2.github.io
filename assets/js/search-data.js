1b07e02930de07fe10988ab9f8568220a282ea57
eesphgos2 <eesphgos2@gmail.com>
2025-01-03 09:47:58 +0000

    Deploying to gh-pages from @ eesphgos2/eesphgos2.github.io@0a7c86caac815a95c25e91c896e631be8e33d6f5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -133,7 +133,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%65%65%73%70%68%67%6F%73@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
         id: 'social-github',
